2fee45ead0b2b4a0f080e3c9dff47f8a46ee8919
Waitsnake <942085+Waitsnake@users.noreply.github.com>
2023-05-16 19:11:13 +0200

    ch4

--- pixelate.js
+++ pixelate.js
@@ -1,4 +1,15 @@
 "use strict";
+var PIXEL_RATIO = (function () {
+    var ctx = document.createElement("canvas").getContext("2d"),
+        dpr = window.devicePixelRatio || 1,
+        bsr = ctx.webkitBackingStorePixelRatio ||
+              ctx.mozBackingStorePixelRatio ||
+              ctx.msBackingStorePixelRatio ||
+              ctx.oBackingStorePixelRatio ||
+              ctx.backingStorePixelRatio || 1;
+
+    return dpr / bsr;
+})();
 var PixelateHelper = {
     ready: function (fn) {
         if (document.readyState != "loading") {

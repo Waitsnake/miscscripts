6cd1e5978404e7df75680bd817f4eac7733822b5
Waitsnake <942085+Waitsnake@users.noreply.github.com>
2023-05-16 20:10:08 +0200

    c

--- pixelate.js
+++ pixelate.js
@@ -135,7 +135,7 @@ var PixelateHelper = {
             this.canvasCrop.width = this.displayWidth;
             this.canvasCrop.height = this.displayHeight;
             this.canvasCrop.style.display = "none";
-            this.canvasCrop.getContext("2d").setTransform(this.ratio, 0, 0, this.ratio, 0, 0);
+//            this.canvasCrop.getContext("2d").setTransform(this.ratio, 0, 0, this.ratio, 0, 0);
             this.contextCrop = this
                 .canvasCrop
                 .getContext("2d");
